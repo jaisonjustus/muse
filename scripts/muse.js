aceaf1eabbb62186b61dba390458c4e93b58cc8c
Jaison Justus <jaison.justus.lp@gmail.com>
2013-09-18 20:05:30 +0530

    Add Muse Script.
    Add Guitar, Core, Filter code blocks. Guitar Not yet tested.
    Add Equalizer code.. not tested also :beers:

--- scripts/muse.js
+++ scripts/muse.js
@@ -11,6 +11,14 @@ var Filters = Muse.Filters = {
   'allpass' : 7
 };
 
+var Selectors = Muse.Selectors = {
+  distortion : $('#distortion'),
+  bass : $('#bass'),
+  treble : $('#treble'),
+  delay : $('#delay'),
+  gain : $('#gain')
+};
+
 var Core = Muse.Core = {
   context : new window.webkitAudioContext(),
 
@@ -26,26 +34,126 @@ var Core = Muse.Core = {
   },
 
   createDelayNode : function(time)  {
-    var delay = Muse.Core.context.createDelayNode();
+    var delay = Muse.Core.context.createDelayNode(10);
 
     delay.delayTime.value = time;
 
     return delay;
+  },
+
+  distortionForm : function(samples, amount) {
+    if (typeof amount === 'undefined') { amount = 0.1; }
+
+    var k = 2 * amount/ (1 - amount),
+        curve = new Float32Array(samples);
+
+    for (var i = 0; i < samples; i++) {
+      var x = (i - 0) * (1 - (-1)) / (samples - 0) + (-1);
+      curve[i] = (1 + k) * x / (1+ k * Math.abs(x));
+    }
+
+    return curve;
   }
+
 };
 
-var Player = Muse.Player = {
+var Guitar = Muse.Guitar = {
 
   nodes : {
     distortion : Muse.Core.context.createWaveShaper(),
-    bass : Muse.Core.createNode('lowshelf', 440, 0, 0),
-    treble : Muse.Core.createNode('highshelf', 1700, 0, 0),
+    bass : Muse.Core.createNode('lowshelf', 440, 0, Muse.Selectors.bass),
+    treble : Muse.Core.createNode('highshelf', 1700, 0, Muse.Selectors.treble),
     delay : Muse.Core.createDelayNode(0),
     gain : Muse.Core.context.createGainNode()
   },
 
+  eventBindings : function()  {
+    var that = this;
+    console.log('eventBindings');
+
+    Muse.Selectors.gain.dial({'change': function (v) {
+      that.nodes.gain.gain.value = v / 100;
+    }});
+
+    Muse.Selectors.distortion.dial({'change': function (v) {
+      that.nodes.distortion.curve = Muse.Core.distortionForm(Muse.Core.context.sampleRate, v / 100);
+    }});
+
+    Muse.Selectors.bass.dial({'change': function (v) {
+      that.nodes.bass.gain.value = v / 100;
+    }});
+
+    Muse.Selectors.treble.dial({'change': function (v) {
+      that.nodes.treble.gain.value = v / 100;
+    }});
+  },
+
   init : function() {
-    $('.panel__knob').knob();
+    $('.panel__equalizer').bars();
+    this.eventBindings();
+  },
+
+  streaming : function(stream) {
+    var guitar = Muse.Core.context.createMediaStreamSource(stream);
+
+    Muse.Guitar.nodes.gain.gain.value = $('#gain').val() / 100;
+
+    Muse.Guitar.nodes.distortion.curve = Muse.Core.distortionForm(Muse.Core.context.sampleRate)
+
+    guitar.connect(Muse.Guitar.nodes.distortion);
+    Muse.Guitar.nodes.distortion.connect(Muse.Guitar.nodes.bass);
+    Muse.Guitar.nodes.bass.connect(Muse.Guitar.nodes.treble);
+    Muse.Guitar.nodes.treble.connect(Muse.Guitar.nodes.delay);
+    Muse.Guitar.nodes.delay.connect(Muse.Guitar.nodes.gain);
+    Muse.Guitar.nodes.gain.connect(Muse.Core.context.destination)
+  }
+
+}
+
+var Equalizer = Muse.Equalizer = {
+
+  nodes : [],
+
+  filter : 'bandpass',
+
+  band : [],
+
+  init : function(band) {
+    this.band = band;
+    this.createEquializerNode();
+    this.createEquializer();
+  },
+
+  createEquializerNode : function() {
+    var filter = null;
+
+    for(var i = 0; i < this.band.length; i++)  {
+      filter = new Muse.Core.context.createBiquadFilter()
+      filter.type = Muse.Filters[this.filter];
+      filter.frequency.value = this.band[i];
+      filter.Q.value = 1;
+      filter.gain.value = 1;
+
+      nodes.push(filter);
+    }
+  },
+
+  createEquializer : function() {
+    for(var i = 0, j = 1; j < this.nodes.length; i++, j++)  {
+      this.nodes[i].connect(this.nodes[j]);
+    }
+  },
+
+  modifyFrequency : function(band, freq)  {
+    this.nodes[band] = freq;
+  },
+
+  endPoint : function(start)  {
+    if(start) {
+      return this.nodes[0];
+    }else {
+      return this.nodes[this.nodes.length - 1];
+    }
   }
 
 }
